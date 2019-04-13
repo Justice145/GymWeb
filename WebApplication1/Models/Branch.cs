6a2e13c916ed882d3aaf6b7378662f0190593a0c
matanix <matanppp531@gmail.com>
2019-04-13 20:59:50 +0300

    Change the opening hour format

--- WebApplication1/Models/Branch.cs
+++ WebApplication1/Models/Branch.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebApplication1.Models
 {
@@ -10,11 +11,18 @@ namespace WebApplication1.Models
         public int Id { get; set; }
         public string Address { get; set; }
         public string PhoneNumber { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:hh\\:mm}", ApplyFormatInEditMode = true)]
         public TimeSpan WeekDayOpen { get; set; }
+        [DisplayFormat(DataFormatString = "{0:hh\\:mm}", ApplyFormatInEditMode = true)]
         public TimeSpan WeekDayClose { get; set; }
+        [DisplayFormat(DataFormatString = "{0:hh\\:mm}", ApplyFormatInEditMode = true)]
         public TimeSpan FridayOpen { get; set; }
+        [DisplayFormat(DataFormatString = "{0:hh\\:mm}", ApplyFormatInEditMode = true)]
         public TimeSpan FridayClose { get; set; }
+        [DisplayFormat(DataFormatString = "{0:hh\\:mm}", ApplyFormatInEditMode = true)]
         public TimeSpan SaturdayOpen { get; set; }
+        [DisplayFormat(DataFormatString = "{0:hh\\:mm}", ApplyFormatInEditMode = true)]
         public TimeSpan SaturdayClose { get; set; }
     }
 }
