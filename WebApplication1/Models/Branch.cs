e8ae91635063766d127d432c34bdf977d7537714
matanix <matanppp531@gmail.com>
2019-04-13 20:39:44 +0300

    Added branches model, controller and basic views.

---
+++ WebApplication1/Models/Branch.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Branch
+    {
+        public int Id { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public TimeSpan WeekDayOpen { get; set; }
+        public TimeSpan WeekDayClose { get; set; }
+        public TimeSpan FridayOpen { get; set; }
+        public TimeSpan FridayClose { get; set; }
+        public TimeSpan SaturdayOpen { get; set; }
+        public TimeSpan SaturdayClose { get; set; }
+    }
+}
